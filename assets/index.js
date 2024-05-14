30477cb1fb55a2f56b9b4e502c09cc90885593b3
Flash_Man <flashman666@qq.com>
2024-05-14 22:20:57 +0800

    添加blur效果

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,9 @@
 function float() {
 	document.getElementById("WindowIntro").className = "window intro active"
+	document.getElementById("Wrapper").className = "wrapper hidden"
 }
 
 function down() {
 	document.getElementById("WindowIntro").className = "window intro"
+	document.getElementById("Wrapper").className = "wrapper"
 }
